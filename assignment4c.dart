2f78f4ba1f0537c028eadf5ef436b9260889f99a
KenApollo <kgicheha15670@gmail.com>
2025-07-11 16:47:28 +0300

    wassup

--- assignment4c.dart
+++ assignment4c.dart
@@ -1,1 +1,29 @@
 //largest number of 3 
+
+import 'dart:io';
+
+void main (){
+  //now for the 3 numbers ...
+  print('Put in the first number');
+  int num1=int.parse(stdin.readLineSync()!);
+
+  print('Put in the second number');
+  int num2=int.parse(stdin.readLineSync()!);
+
+  print('Put in the third number');
+  int num3=int.parse(stdin.readLineSync()!);
+
+  if ((num1>num2) && (num1>num3)){
+    print('$num1 is the largest number');
+
+  }else if ((num1<num2) && (num1>num3)){
+    print('$num2 is the largest number');
+  }else{
+    print('$num3 is the biggest number');
+  }
+
+
+
+
+
+}
